4366ae9b7afc453d35f8443bd51d201d521fbb97
René Pasold <rene.pasold@jena.de>
2020-10-01 11:27:58 +0200

    Revert to previous value

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1011,7 +1011,7 @@
 #define XY_PROBE_SPEED (133*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST HOMING_FEEDRATE_Z
+#define Z_PROBE_SPEED_FAST (20*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_SPEED_SLOW (Z_PROBE_SPEED_FAST / 2)
