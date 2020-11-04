a8fae43ad858f956328170c6074c69298a0301cd
René Pasold <rene@pasold.org>
2020-11-04 20:27:55 +0100

    IR Probe

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -17.5, -47, -1.20 }
+#define NOZZLE_TO_PROBE_OFFSET { -23, -8, -3 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
